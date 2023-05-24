37f667dd65cf82ea6f63f74acecf8bc4d95d9d0d
marjanalbouye <marjan.albouye73@gmail.com>
2023-05-24 12:38:00 -0600

    Add flow code.

---
+++ flow/init.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+"""Initialize the project's data space.
+Iterates over all defined state points and initializes
+the associated job workspace directories.
+The result of running this file is the creation of a signac workspace:
+    - signac.rc file containing the project name
+    - signac_statepoints.json summary for the entire workspace
+    - workspace/ directory that contains a sub-directory of every individual statepoint
+    - signac_statepoints.json within each individual statepoint sub-directory.
+"""
+
+import logging
+from collections import OrderedDict
+from itertools import product
+
+import signac
+
+
+def get_parameters():
+    parameters = OrderedDict()
+
+    # project parameters
+    parameters["project"] = ["LJ-single-particle"]
+    parameters["group"] = ["force"]
+    parameters["notes"] = ["Learning LJ force curve using positions"]
+    parameters["tags"] = [["PPS", "NN", "force", "fixedNN"]]
+
+    # dataset parameters
+    parameters["data_path"] = ["/home/marjanalbooyeh/code/ML_LJ/datasets/single_particle"]
+    parameters["inp_mode"] = ["pos", "r"]
+    parameters["batch_size"] = [64]
+
+    # model parameters
+    parameters["model_type"] = ["fixed"]
+    parameters["hidden_dim"] = [32, 64]
+    parameters["n_layer"] = [2, 10]
+    parameters["act_fn"] = ["Tanh", "LeakyReLU"]
+    parameters["dropout"] = [0.5]
+
+    # optimizer parameters
+    parameters["optim"] = ["Adam"]
+    parameters["lr"] = [0.5]
+    parameters["decay"] = [0.0001]
+
+    # run parameters
+    parameters["epochs"] = [1000]
+
+    return list(parameters.keys()), list(product(*parameters.values()))
+
+
+custom_job_doc = {}  # add keys and values for each job document created
+
+
+def main(root=None):
+    project = signac.init_project("ML-LJ", root=root)  # Set the signac project name
+    param_names, param_combinations = get_parameters()
+    # Create jobs
+    for params in param_combinations:
+        parent_statepoint = dict(zip(param_names, params))
+        parent_job = project.open_job(parent_statepoint)
+        parent_job.init()
+        parent_job.doc.setdefault("done", False)
+
+    project.write_statepoints()
+    return project
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    main()
